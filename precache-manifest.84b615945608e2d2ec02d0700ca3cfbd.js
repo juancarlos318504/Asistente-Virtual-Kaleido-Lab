5fad2e7d4e865bc30b2d3455d8785bf4cdb03a51
JuanCarlos <juancarlos318504@gmail.com>
2020-09-15 13:29:50 -0500

    Updates

---
+++ precache-manifest.84b615945608e2d2ec02d0700ca3cfbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408b13d2b43f82644e853d45d5ab5530",
+    "url": "/Asistente-Virtual-Kaleido-Lab/index.html"
+  },
+  {
+    "revision": "858e7e663de21d6a2971",
+    "url": "/Asistente-Virtual-Kaleido-Lab/static/css/main.1f7a4c1e.chunk.css"
+  },
+  {
+    "revision": "6b2c23d75ef4e81d544d",
+    "url": "/Asistente-Virtual-Kaleido-Lab/static/js/2.e8445213.chunk.js"
+  },
+  {
+    "revision": "df1642c39f4a8aa7ea15f32eff69fe15",
+    "url": "/Asistente-Virtual-Kaleido-Lab/static/js/2.e8445213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858e7e663de21d6a2971",
+    "url": "/Asistente-Virtual-Kaleido-Lab/static/js/main.be317b91.chunk.js"
+  },
+  {
+    "revision": "fb08d7b6b006f39efe65",
+    "url": "/Asistente-Virtual-Kaleido-Lab/static/js/runtime-main.ec0a0b2f.js"
+  },
+  {
+    "revision": "32ed4a809b49ef04c0cb882ee6a66b19",
+    "url": "/Asistente-Virtual-Kaleido-Lab/static/media/Logo.32ed4a80.png"
+  }
+]);
